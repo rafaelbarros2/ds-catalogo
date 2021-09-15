a58a33eaebab0ab2373773b0b2657dcbb4a24610
lfelipe <rafael.eng.mecanic@gmail.com>
2021-09-15 20:28:20 -0300

    Pages

--- backend/src/main/java/com/rafaelb/dscatalog/entities/Category.java
+++ backend/src/main/java/com/rafaelb/dscatalog/entities/Category.java
@@ -6,13 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -28,4 +32,20 @@ public class Category implements Serializable {
     private Long id;
     @NotBlank(message = "Preencha o campo name.")
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 }
--- backend/src/main/java/com/rafaelb/dscatalog/resouces/CategoryResource.java
+++ backend/src/main/java/com/rafaelb/dscatalog/resouces/CategoryResource.java
@@ -3,6 +3,9 @@ package com.rafaelb.dscatalog.resouces;
 import com.rafaelb.dscatalog.dtos.CategoryDTO;
 import com.rafaelb.dscatalog.services.CategoyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,8 +32,14 @@ public class CategoryResource {
     private CategoyService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+                                                     @RequestParam(value = "direction", defaultValue = "DESC") String direction,
+                                                     @RequestParam(value = "orderBy", defaultValue = "name") String orderBy)
+    {
+
+        PageRequest pageRequest =PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
