3ec557456f3e233a790d3126053cd16256eea9d2
lfelipe <rafael.eng.mecanic@gmail.com>
2021-10-04 19:19:16 -0300

    refatoração

--- backend/src/main/java/com/rafaelb/dscatalog/exceptions/DataBaseExcptions.java
+++
@@ -1,7 +0,0 @@
-package com.rafaelb.dscatalog.exceptions;
-
-public class DataBaseExcptions extends RuntimeException{
-    public DataBaseExcptions(String msg) {
-        super(msg);
-    }
-}
--- backend/src/main/java/com/rafaelb/dscatalog/exceptions/ResourceNotFoundExcptions.java
+++
@@ -1,7 +0,0 @@
-package com.rafaelb.dscatalog.exceptions;
-
-public class ResourceNotFoundExcptions extends RuntimeException{
-    public ResourceNotFoundExcptions(String msg) {
-        super(msg);
-    }
-}
