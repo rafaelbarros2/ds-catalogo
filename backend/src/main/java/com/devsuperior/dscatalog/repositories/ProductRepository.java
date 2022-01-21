386d0278c21d26658fbbfd044dcaa08b54ad061f
lfelipe <rafael.eng.mecanic@gmail.com>
2022-01-21 19:30:50 -0300

    Resolução do problema do n+1 consultas

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -19,4 +19,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             + "(COALESCE(:category) IS NULL OR :category IN :categories) AND "
             + "LOWER(obj.name LIKE LOWER (CONCAT('% ', :name, '%')) ) AND")
     Page<Product> findSort(List<Category> categories, Pageable pageable, String name);
+
+    @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+    List<Category> findProductsWithCategories(List<Product> products);
 }
