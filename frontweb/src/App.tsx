4dfae8f328a0af9fcfd29f13dc5dcd6249adfa83
lfelipe <rafael.eng.mecanic@gmail.com>
2022-03-16 14:28:50 -0300

    responsitividade até o brackpoint 992px

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,13 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
 import NavBar from 'components/NavBar';
+import Home from 'pages/Home';
 
 const App = () => {
   return(
-    <>
-    <NavBar/>
-  <h1>Hello React</h1>
-  </>
+    <Home/>
   );
 }
 
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import{ReactComponent as MainImage } from  'assets/images/main-image.svg'
+import ButtonIcon from 'components/ButtonIcon';
+import NavBar from 'components/NavBar';
+import './styles.css'
+const Home = () => {
+  return(
+    <>
+    <NavBar/>
+    <div className="home-container"> 
+        <div className="home-card">
+            <div className="home-content-container">
+                <h1>Conheça o melhor catálago de produtos</h1>
+                <p>Ajudaremos você a encontrar os melhores produtos disponiveis do mercado</p>
+                <ButtonIcon/>
+            </div>
+            <div className="home-image-container">
+                <MainImage/>
+            </div>
+        </div>
+    </div>
+  </>
+  );
+}
+
+export default Home;
